6bd98345cb7b611e2411b577d0b6edcec5a31c6a
Corentin Poupry <helloedit@archlinux-hp.localdomain>
2020-12-02 22:29:51 +0100

    day 02

---
+++ day02/ex2.py
@@ -0,0 +1,42 @@
+import re
+from collections import Counter, namedtuple
+from typing import Dict, List
+
+
+Row = namedtuple("Policy", ["lower", "higher", "letter", "password"])
+policy_regex = re.compile(
+    r"^(?P<lower>\d+)-(?P<higher>\d+) (?P<letter>\w): (?P<password>\w+)$")
+
+
+def ex2(dataset: List[Row]):
+    """Count correct password according to the policy."""
+    correct = 0
+    for data in dataset:
+        interval = range(int(data.lower), int(data.higher) + 1)
+        counter = Counter(data.password)
+
+        if counter.get(data.letter, 0) in interval:
+            correct += 1
+
+    print("1st: {}".format(correct))
+
+
+def ex2bis(dataset: List[Row]):
+    """Count correct password according to the 'new' policy."""
+    correct = 0
+    for data in dataset:
+        letter = data.letter
+        password = data.password
+        if (password[int(data.lower) - 1] == letter) ^ (password[int(data.higher) - 1] == letter):
+            correct += 1
+
+    print("2nd: {}".format(correct))
+
+
+if __name__ == "__main__":
+    dataset = None
+    with open("./day02/input.txt") as file:
+        dataset = [Row(**policy_regex.match(x).groupdict()) for x in file.readlines()]
+
+    ex2(dataset)
+    ex2bis(dataset)
